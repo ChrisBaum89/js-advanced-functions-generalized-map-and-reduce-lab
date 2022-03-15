09634dd72f01c2a6e09bde250bbb3985a93f9c80
Chris Baum <Christopher.Baum@valpo.edu>
2022-03-14 21:18:44 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Add your functions here
+
+function map (sourceArray, callback){
+  const map1 = sourceArray.map(callback)
+  return map1
+}
+
+function reduce (sourceArray, callback, initialValue){
+  let reduce1
+  if (Number.isInteger(initialValue)){
+    reduce1 = sourceArray.reduce((previousValue, currentValue) => callback(previousValue, currentValue), initialValue)
+  }
+  else {
+    reduce1 = sourceArray.reduce((previousValue, currentValue) => callback(previousValue, currentValue))
+  }
+
+  return reduce1
+}
